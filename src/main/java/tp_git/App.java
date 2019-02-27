838a1067373f88f396b5e39c46e26a63b1f0fc99
Kyross <vincent.berthet42@gmail.com>
2019-02-27 15:38:09 +0100

    Add calcultor feature (#1)
    Normal mode only
    
    This commit does the following:
    
    * add a calculator with normal mode

--- src/main/java/tp_git/App.java
+++ src/main/java/tp_git/App.java
@@ -1,5 +1,9 @@
 package tp_git;
 
+import org.apache.commons.math3.analysis.function.Add;
+import org.apache.commons.math3.analysis.function.Divide;
+import org.apache.commons.math3.analysis.function.Multiply;
+
 import java.util.Scanner;
 
 /**
@@ -8,16 +12,47 @@ import java.util.Scanner;
  */
 public class App 
 {
+    static final int NORMAL_MODE=1;
+    static final int SCIENTIFIC_MODE=2;
+    static final int ADDITION=1;
+    static final int SUBTRACTION=2;
+    static final int MULTIPLY=3;
+    static final int DIVIDE=4;
+
     public static void main( String[] args )
     {
         Scanner in=new Scanner(System.in);
-        final int NORMAL_MODE=1;
-        final int SCIENTIFIC_MODE=2;
         int mode;
-        do {
-            System.out.println("Select mode : \n 1-Normal \n 2-Scientific");
-            mode = in.nextInt();
-       }while(mode!=NORMAL_MODE && mode!=SCIENTIFIC_MODE);
+
+        System.out.println("Select mode : \n 1-Normal \n 2-Scientific");
+        mode = in.nextInt();
+        if(mode==NORMAL_MODE) normalMode(in);
+        if(mode==SCIENTIFIC_MODE) scientificMode(in);
+        System.out.println("End");
+    }
+
+    public static void normalMode(Scanner in){
+        System.out.println("Action normal mode : " +
+                "\n "+ADDITION+"-Addition " +
+                "\n "+SUBTRACTION+"-Subtraction " +
+                "\n "+MULTIPLY+"-Multiply " +
+                "\n "+DIVIDE+"-Divide");
+
+        int action = in.nextInt();
+        System.out.println("****");
+        double a = in.nextDouble();
+        double b= in.nextDouble();
+
+        if(action==ADDITION) System.out.println("="+new Add().value(a,b));
+        if(action==SUBTRACTION) System.out.println("="+new Add().value(a,b));
+        if(action==MULTIPLY) System.out.println("="+new Multiply().value(a,b));
+        if(action==DIVIDE) System.out.println("="+new Divide().value(a,b));
 
     }
+
+    public static void scientificMode(Scanner in){
+
+    }
+
+
 }
