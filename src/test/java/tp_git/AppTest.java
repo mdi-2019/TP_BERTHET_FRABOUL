db74d6e32e123b842c0649a6588d8eec6165dbe6
Vincent <vincent.berthet@etudiant.univ-rennes1.fr>
2019-02-27 16:00:31 +0100

    add multiply test

--- src/test/java/tp_git/AppTest.java
+++ src/test/java/tp_git/AppTest.java
@@ -2,6 +2,7 @@ package tp_git;
 
 import static org.junit.Assert.assertTrue;
 
+import org.apache.commons.math3.analysis.function.Multiply;
 import org.junit.Test;
 
 /**
@@ -16,5 +17,11 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void opBinaire(){
+        double multiply=new Multiply().value(4,4);
+        assertTrue( "multiply",multiply==16);
     }
 }
